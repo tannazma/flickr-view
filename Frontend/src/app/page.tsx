8891eaa3c04789fb4e766845c09e7de8013aa739
tannazma <97254921+tannazma@users.noreply.github.com>
2024-04-27 16:38:24 +0200

    show the tags for the items  and make the tags seperate from + styling a bit

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -49,21 +49,23 @@ export default function Home() {
         })}`;
 
         return (
-        <div
-          key={index}
-          className="border border-gray-300 rounded-lg overflow-hidden shadow-sm w-72"
-        >
-          <div className="relative w-full h-48">
-            <Image
-              src={pic.imageURL}
-              alt={pic.title}
-              layout="fill"
-              objectFit="cover"
+          <div
+            key={index}
+            className="border border-gray-300 rounded-lg overflow-hidden shadow-sm w-72"
+          >
+            <div className="relative w-full h-48">
+              <Image
+                src={pic.imageURL}
+                alt={pic.title}
+                layout="fill"
+                objectFit="cover"
                 className="rounded-t-lg overflow-hidden"
-            />
-          </div>
+              />
+            </div>
             <div className="p-2">
-              <h3 className="text-lg font-semibold overflow-hidden">{pic.title}</h3>
+              <h3 className="text-lg font-semibold overflow-hidden">
+                {pic.title}
+              </h3>
               <div className="text-sm text-gray-600 overflow-hidden line-clamp-4">
                 <strong>Author: </strong>
                 {pic.author.match(/"([^"]*)"/)?.[1] || "No author found"}
@@ -72,16 +74,34 @@ export default function Home() {
                 <strong className="text-sm text-gray-500">Published: </strong>
                 <span className="text-sm text-gray-500">{formattedDate}</span>
               </div>
-            <a
-              href={pic.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block mt-2 text-blue-600 hover:text-blue-800 hover:underline"
-            >
-              View on Flickr
-            </a>
+              {pic.tags && (
+                <div className="text-sm text-gray-500">
+                  <strong>Tags: </strong>
+                  <div>
+                    {pic.tags
+                      .split(" ")
+                      .slice(0, 4)
+                      .map((tag) => (
+                        <span
+                          key={tag}
+                          className="p-1 m-1 inline-block bg-gray-200 rounded overflow-hidden"
+                        >
+                          {tag}
+                        </span>
+                      ))}
+                  </div>
+                </div>
+              )}
+              <a
+                href={pic.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block mt-2 text-blue-600 hover:text-blue-800 hover:underline"
+              >
+                View on Flickr
+              </a>
+            </div>
           </div>
-        </div>
         );
       })}
     </div>
