5d32655d6a805cac78fbfdef21c8cb91baf43000
tannazma <97254921+tannazma@users.noreply.github.com>
2024-04-27 12:36:50 +0200

    pass simplifiedData to data before sending it back

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import fetch from "node-fetch";
+import { title } from "process";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,19 +10,34 @@ app.get("/", (req, res) => {
 });
 
 // Route to fetch images
+// I have 2 functionalities for my API: to fetch all images: /api/photos and fetch based on the tages: /api/photos?tags="something"
 app.get("/api/photos", async (req: Request, res: Response) => {
-  const tags = req.query.tags;
+  const tags = req.query.tags || "";
   const apiUrlForFetchingImages = `https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=${encodeURIComponent(
     String(tags)
   )}`;
+  // console.log(apiUrlForFetchingImages);
 
   try {
     const response = await fetch(apiUrlForFetchingImages);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json();
-    res.json(data);
+    const data: any = await response.json();
+
+    // simplify the data before sending it back
+    const simplifiedData = data.items.map((item: any) => ({
+      title: item.title,
+      imageURL: item.media.m,
+      author: item.author,
+      link: item.link,
+      description: item.description,
+      tags: item.tags,
+    }));
+    // console.log("simplifiedData", simplifiedData);
+    console.log("data", data);
+
+    res.json(simplifiedData);
   } catch (error: any) {
     console.error("Failed to fetch data:", error);
     res
