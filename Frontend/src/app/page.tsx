7b459635fe085ba27bc6fcd6f47e799a06ac263b
tannazma <97254921+tannazma@users.noreply.github.com>
2024-04-29 00:43:27 +0200

    Merg PhotoCard + improve getPictures function

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,41 +1,31 @@
 "use client";
 
-import { useState } from "react";
-import Image from "next/image";
-import PhotoCards from "./components/PhotoCard";
+import { useEffect, useState } from "react";
+import { Picture } from "./types";
+import { PhotoCard } from "./components/PhotoCard";
 
-type Pictures = {
-  title: string;
-  imageURL: string;
-  author: string;
-  link: string;
-  description: string;
-  tags: string;
-  published: string;
-};
 export default function Home() {
-  const [query, setQuery] = useState("");
-  const [pictures, setPictures] = useState<Pictures[]>([]);
-  const [filteredPictures, setFilteredPictures] = useState(false);
+  const [pictures, setPictures] = useState<Picture[]>([]);
 
-  const handleSearchTags = async () => {
+  const getPictures = async () => {
     try {
       const response = await fetch(
         `http://localhost:3000/api/photos?tags=${encodeURIComponent(query)}`
       );
-      if (!response.ok) {
-        throw new Error("Failed to fetch data");
-      }
       const data = await response.json();
-      const filteredData = query
-        ? data.filter((pic: Pictures) => pic.tags.split(" ").includes(query))
-        : data;
-      setPictures(filteredData);
-      setFilteredPictures(true);
+      setPictures(data);
+      if (!response.ok) {
+        throw new Error("Failed to fecth pictures");
+      }
     } catch (error) {
       console.log("Error fetching pictures", error);
     }
   };
+
+  useEffect(() => {
+    getPictures();
+  }, []);
+
   return (
     <div>
       <div className="relative flex align-middle text-center justify-center">
@@ -53,47 +43,8 @@ export default function Home() {
         </button>
       </div>
       <div className="flex flex-wrap justify-around p-7 gap-3 origin-top-left">
-        {filteredPictures &&
-          pictures
-            .filter((item) =>
-              item.tags.toLowerCase().includes(query.toLowerCase())
-            )
-            .map((pic, index) => (
-              <div
-                key={index}
-                className="border border-gray-300 rounded-lg overflow-hidden shadow-sm w-72"
-              >
-                <div className="relative w-full h-48">
-                  <Image
-                    src={pic.imageURL}
-                    alt={pic.title}
-                    layout="fill"
-                    sizes="(max-width: 768px) 100vw"
-                    objectFit="cover"
-                    className="rounded-t-lg overflow-hidden"
-                  />
-                </div>
-                <div className="p-2">
-                  {pic.tags && (
-                    <div className="text-sm text-gray-500">
-                      <strong>Tags: </strong>
-                      <div>
-                        {pic.tags
-                          .split(" ")
-                          .slice(0, 4)
-                          .map((tag) => (
-                            <span
-                              key={tag}
-                              className="p-1 m-1 inline-block bg-gray-200 rounded overflow-hidden"
-                            >
-                              {tag.trim()}
-                            </span>
-                          ))}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
+        {pictures.map((pic, index) => (
+          <PhotoCard key={index} pic={pic} showOnlyTags={false} />
             ))}
       </div>
       {filteredPictures || <PhotoCards photos={pictures} />}
