105f326bfaec6950c47cca1a6d754702462d3c2d
tannazma <97254921+tannazma@users.noreply.github.com>
2024-04-29 00:38:19 +0200

    pass showOnlyTags to PhotoCard component to show tags in default

--- Frontend/src/app/components/PhotoCard.tsx
+++ Frontend/src/app/components/PhotoCard.tsx
@@ -1,115 +1,83 @@
 import Image from "next/image";
-import { ReactElement, useEffect, useState } from "react";
+import { Picture } from "../types";
 
-type Pictures = {
-  title: string;
-  imageURL: string;
-  author: string;
-  link: string;
-  description: string;
-  tags: string;
-  published: string;
+type Props = {
+  pic: Picture;
+  showOnlyTags: boolean;
 };
 
-interface PhotoCardsProps {
-  photos: Pictures[];
-}
-
-const PhotoCards = ({ photos }: PhotoCardsProps): ReactElement => {
-  const [pictures, setPictures] = useState<Pictures[]>([]);
-
-  const getPictures = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/api/photos");
-      const data = await response.json();
-      console.log(data);
-      setPictures(data);
-      if (!response.ok) {
-        throw new Error("Failed to fecth pictures");
-      }
-    } catch (error) {
-      console.log("Error fetching pictures", error);
-    }
-  };
-
-  useEffect(() => {
-    getPictures();
-  }, []);
+export function PhotoCard({ pic, showOnlyTags = false }: Props) {
+  const date = new Date(pic.published);
+  const formattedDate = `${date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })} at ${date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  })}`;
 
   return (
-    <div className="flex flex-wrap justify-around p-7 gap-3 origin-top-left">
-      {pictures.map((pic, index) => {
-        const date = new Date(pic.published);
-        const formattedDate = `${date.toLocaleDateString("en-US", {
-          weekday: "long",
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })} at ${date.toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: true,
-        })}`;
-
-        return (
-          <div
-            key={index}
-            className="border border-gray-300 rounded-lg overflow-hidden shadow-sm w-72"
-          >
-            <div className="relative w-full h-48">
-              <Image
-                src={pic.imageURL}
-                alt={pic.title}
-                layout="fill"
-                sizes="(max-width: 768px) 100vw"
-                objectFit="cover"
-                className="rounded-t-lg overflow-hidden"
-              />
+    <div
+      key={pic.title}
+      className="border border-gray-300 rounded-lg overflow-hidden shadow-sm w-72"
+    >
+      <div className="relative w-full h-48">
+        <Image
+          src={pic.imageURL}
+          alt={pic.title}
+          layout="fill"
+          sizes="(max-width: 768px) 100vw"
+          objectFit="cover"
+          className="rounded-t-lg overflow-hidden"
+        />
+      </div>
+      <div className="p-2">
+        {showOnlyTags ? <></> : (
+          <>
+            <h3 className="text-lg font-semibold overflow-hidden">
+              {pic.title}
+            </h3>
+            <div className="text-sm text-gray-600 overflow-hidden line-clamp-4">
+              <strong>Author: </strong>
+              {pic.author.match(/"([^"]*)"/)?.[1] || "No author found"}
             </div>
-            <div className="p-2">
-              <h3 className="text-lg font-semibold overflow-hidden">
-                {pic.title}
-              </h3>
-              <div className="text-sm text-gray-600 overflow-hidden line-clamp-4">
-                <strong>Author: </strong>
-                {pic.author.match(/"([^"]*)"/)?.[1] || "No author found"}
-              </div>
-              <div>
-                <strong className="text-sm text-gray-500">Published: </strong>
-                <span className="text-sm text-gray-500">{formattedDate}</span>
-              </div>
-              {pic.tags && (
-                <div className="text-sm text-gray-500">
-                  <strong>Tags: </strong>
-                  <div>
-                    {pic.tags
-                      .split(" ")
-                      .slice(0, 4)
-                      .map((tag) => (
-                        <span
-                          key={tag}
-                          className="p-1 m-1 inline-block bg-gray-200 rounded overflow-hidden"
-                        >
-                          {tag.trim()}
-                        </span>
-                      ))}
-                  </div>
-                </div>
-              )}
-              <a
-                href={pic.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="block mt-2 text-blue-600 hover:text-blue-800 hover:underline"
-              >
-                View on Flickr
-              </a>
+            <div>
+              <strong className="text-sm text-gray-500">Published: </strong>
+              <span className="text-sm text-gray-500">{formattedDate}</span>
+            </div>
+          </>
+        )}
+        {pic.tags && (
+          <div className="text-sm text-gray-500">
+            <strong>Tags: </strong>
+            <div>
+              {pic.tags
+                .split(" ")
+                .slice(0, 4)
+                .map((tag) => (
+                  <span
+                    key={tag}
+                    className="p-1 m-1 inline-block bg-gray-200 rounded overflow-hidden"
+                  >
+                    {tag.trim()}
+                  </span>
+                ))}
             </div>
           </div>
-        );
-      })}
+        )}
+        <a
+          href={pic.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block mt-2 text-blue-600 hover:text-blue-800 hover:underline"
+        >
+          View on Flickr
+        </a>
+      </div>
     </div>
   );
-};
-export default PhotoCards;
+}
